0bfc2d5e8ed4833f8f66a40f0a6e5d8057f6fba7
zar4za <zar4z4@yandex.ru>
2024-10-15 00:48:44 +0300

    perf: various optimizations

--- base83.go
+++ base83.go
@@ -1,103 +1,34 @@
 package blursh
 
 import (
-	"math"
 	"strings"
 )
 
 var (
 	digitCharacters = [...]byte{
-		'0',
-		'1',
-		'2',
-		'3',
-		'4',
-		'5',
-		'6',
-		'7',
-		'8',
-		'9',
-		'A',
-		'B',
-		'C',
-		'D',
-		'E',
-		'F',
-		'G',
-		'H',
-		'I',
-		'J',
-		'K',
-		'L',
-		'M',
-		'N',
-		'O',
-		'P',
-		'Q',
-		'R',
-		'S',
-		'T',
-		'U',
-		'V',
-		'W',
-		'X',
-		'Y',
-		'Z',
-		'a',
-		'b',
-		'c',
-		'd',
-		'e',
-		'f',
-		'g',
-		'h',
-		'i',
-		'j',
-		'k',
-		'l',
-		'm',
-		'n',
-		'o',
-		'p',
-		'q',
-		'r',
-		's',
-		't',
-		'u',
-		'v',
-		'w',
-		'x',
-		'y',
-		'z',
-		'#',
-		'$',
-		'%',
-		'*',
-		'+',
-		',',
-		'-',
-		'.',
-		':',
-		';',
-		'=',
-		'?',
-		'@',
-		'[',
-		']',
-		'^',
-		'_',
-		'{',
-		'|',
-		'}',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y', 'Z',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z',
+		'#', '$', '%', '*', '+', ',', '-', '.', ':', ';',
+		'=', '?', '@', '[', ']', '^', '_', '{', '|', '}',
 		'~',
 	}
 )
 
-func Encode83(builder *strings.Builder, n int, length int) {
-	// TODO: probably this also needs refactor
+func Encode83(builder *strings.Builder, value int, length int) {
+	divisor := 1
 
-	for i := 1; i <= length; i++ {
-		digit := int(math.Floor(math.Floor(float64(n)/math.Pow(83, float64(length-i))))) % 83
+	for i := 0; i < length-1; i++ {
+		divisor *= 83
+	}
+
+	for i := 0; i < length; i++ {
+		digit := (value / divisor) % 83
+		divisor /= 83
 		builder.WriteByte(digitCharacters[digit])
 	}
 }
--- blursh.go
+++ blursh.go
@@ -3,7 +3,6 @@ package blursh
 import (
 	"errors"
 	"image"
-	"image/color"
 	"math"
 	"strings"
 )
@@ -25,7 +24,6 @@ func Encode(img image.Image, xComp int, yComp int) (string, error) {
 	}
 
 	size := img.Bounds().Max
-	// bytesPerRow := bytesPerPixel * width
 	factors := make([]factor, xComp*yComp)
 
 	for i := range factors {
@@ -48,9 +46,9 @@ func Encode(img image.Image, xComp int, yComp int) (string, error) {
 			actualMaximumValue = math.Max(math.Max(math.Max(factor.r, factor.g), factor.b), actualMaximumValue)
 		}
 
-		quantisedMaximumValue := math.Floor(math.Max(0, math.Min(82, math.Floor(actualMaximumValue*166-0.5))))
-		maximumValue = (quantisedMaximumValue + 1) / 166
-		Encode83(&builder, int(quantisedMaximumValue), 1)
+		quantisedMaximumValue := maxInt(0, minInt(82, int(math.Floor(actualMaximumValue*166-0.5))))
+		maximumValue = float64(quantisedMaximumValue+1) / 166
+		Encode83(&builder, quantisedMaximumValue, 1)
 	} else {
 		Encode83(&builder, 0, 1)
 	}
@@ -72,13 +70,21 @@ func multiplyBasisFunction(xComp int, yComp int, width int, height int, img imag
 		normalisation = 1.
 	}
 
+	cosXs := make([]float64, width)
+
+	for x := range cosXs {
+		cosXs[x] = math.Cos(math.Pi * float64(xComp) * float64(x) / float64(width))
+	}
+
 	for y := 0; y < height; y++ {
+		cosY := math.Cos(math.Pi * float64(yComp) * float64(y) / float64(height))
+
 		for x := 0; x < width; x++ {
-			basis := math.Cos(math.Pi*float64(xComp)*float64(x)/float64(width)) * math.Cos(math.Pi*float64(yComp)*float64(y)/float64(height))
-			rgba := color.RGBAModel.Convert(img.At(x, y)).(color.RGBA)
-			result.r += basis * sRGBToLinear[rgba.R]
-			result.g += basis * sRGBToLinear[rgba.G]
-			result.b += basis * sRGBToLinear[rgba.B]
+			basis := cosXs[x] * cosY
+			r, g, b, _ := img.At(x, y).RGBA()
+			result.r += basis * sRGBToLinear[uint8(r>>8)]
+			result.g += basis * sRGBToLinear[uint8(g>>8)]
+			result.b += basis * sRGBToLinear[uint8(b>>8)]
 		}
 	}
 
@@ -101,10 +107,9 @@ func linearTosRGB(value float64) int {
 	v := math.Max(0, math.Min(1, value))
 
 	if v <= 0.0031308 {
-		return int(math.Trunc(v*12.92*255 + 0.5))
-	} else {
-		return int(math.Trunc((1.055*math.Pow(v, 1/2.4)-0.055)*255 + 0.5))
+		return int(v*12.92*255 + 0.5)
 	}
+	return int((1.055*math.Pow(v, 1/2.4)-0.055)*255 + 0.5)
 }
 
 func encodeDC(value factor) int {
@@ -128,4 +133,18 @@ func signPow(val float64, exp float64) float64 {
 	}
 
 	return sign * math.Pow(math.Abs(val), exp)
+}
+
+func maxInt(left int, right int) int {
+	if left > right {
+		return left
+	}
+	return right
+}
+
+func minInt(left int, right int) int {
+	if left < right {
+		return left
+	}
+	return right
 }
