cb3fca9cc9062a79bcb2b6317a4fd28fd1748906
zar4za <zar4z4@yandex.ru>
2024-10-19 13:37:08 +0300

    perf: specific color conversion of colormodels

--- blursh.go
+++ blursh.go
@@ -3,6 +3,7 @@ package blursh
 import (
 	"errors"
 	"image"
+	"image/color"
 	"math"
 	"strings"
 )
@@ -140,13 +141,41 @@ func imageToPixels(img image.Image) (pixels []pixel, width int, height int) {
 	maxPoint := img.Bounds().Max
 	pixels = make([]pixel, maxPoint.X*maxPoint.Y)
 
-	for y := 0; y < maxPoint.Y; y++ {
-		for x := 0; x < maxPoint.X; x++ {
-			r, g, b, _ := img.At(x, y).RGBA()
-			pixels[y*maxPoint.X+x] = pixel{
-				r: uint8(r >> 8),
-				g: uint8(g >> 8),
-				b: uint8(b >> 8),
+	switch img := img.(type) {
+	case *image.RGBA:
+		for y := 0; y < maxPoint.Y; y++ {
+			for x := 0; x < maxPoint.X; x++ {
+				i := img.PixOffset(x, y)
+				s := img.Pix[i : i+3 : i+3]
+				pixels[y*maxPoint.X+x] = pixel{
+					r: s[0],
+					g: s[1],
+					b: s[2],
+				}
+			}
+		}
+	case *image.YCbCr:
+		for y := 0; y < maxPoint.Y; y++ {
+			for x := 0; x < maxPoint.X; x++ {
+				yi := img.YOffset(x, y)
+				ci := img.COffset(x, y)
+				r, g, b := color.YCbCrToRGB(img.Y[yi], img.Cb[ci], img.Cr[ci])
+				pixels[y*maxPoint.X+x] = pixel{
+					r: r,
+					g: g,
+					b: b,
+				}
+			}
+		}
+	default:
+		for y := 0; y < maxPoint.Y; y++ {
+			for x := 0; x < maxPoint.X; x++ {
+				r, g, b, _ := img.At(x, y).RGBA()
+				pixels[y*maxPoint.X+x] = pixel{
+					r: uint8(r >> 8),
+					g: uint8(g >> 8),
+					b: uint8(b >> 8),
+				}
 			}
 		}
 	}
